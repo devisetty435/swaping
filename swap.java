3cbb9166f00170442cfa0251d8e82497b954508a
devisetty435 <ddevi7028@gmail.com>
2025-06-03 15:32:15 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class swap
+{
+	public static void main(String[] args) {
+		int temp=0;
+		int a=5,b=8;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+" "+b);
+		
+		
+}
+}
